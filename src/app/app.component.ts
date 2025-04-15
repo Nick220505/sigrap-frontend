28c91c73895a942f781ecd0f966da2342f53b3e2
Nick220505 <juannicolaspardo@gmail.com>
2025-04-15 16:32:57 -0500

    Refactor app.component.html and app.component.ts to integrate PrimeNG Button component, replacing the existing header with a card layout for improved UI presentation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [ButtonModule],
 })
 export class AppComponent {}
