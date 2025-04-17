670b0d55d8658a37a432bf222ffbee92e66e8395
Nick220505 <juannicolaspardo@gmail.com>
2025-04-17 03:11:16 -0500

    Remove unused Angular component, configuration, and routing files to streamline project structure and improve maintainability.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,51 +4,51 @@ export const appRoutes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import('./app/layout/component/layout/layout.component').then(
+      import('./layout/component/layout/layout.component').then(
         (m) => m.LayoutComponent,
       ),
     children: [
       {
         path: '',
         loadComponent: () =>
-          import('./app/pages/dashboard/dashboard.component').then(
+          import('./pages/dashboard/dashboard.component').then(
             (m) => m.DashboardComponent,
           ),
       },
       {
         path: 'uikit',
-        loadChildren: () => import('./app/pages/uikit/uikit.routes'),
+        loadChildren: () => import('./pages/uikit/uikit.routes'),
       },
       {
         path: 'documentation',
         loadComponent: () =>
-          import('./app/pages/documentation/documentation.component').then(
+          import('./pages/documentation/documentation.component').then(
             (m) => m.DocumentationComponent,
           ),
       },
       {
         path: 'pages',
-        loadChildren: () => import('./app/pages/pages.routes'),
+        loadChildren: () => import('./pages/pages.routes'),
       },
     ],
   },
   {
     path: 'landing',
     loadComponent: () =>
-      import('./app/pages/landing/landing.component').then(
+      import('./pages/landing/landing.component').then(
         (m) => m.LandingComponent,
       ),
   },
   {
     path: 'notfound',
     loadComponent: () =>
-      import('./app/pages/not-found/not-found.component').then(
+      import('./pages/not-found/not-found.component').then(
         (m) => m.NotFoundComponent,
       ),
   },
   {
     path: 'auth',
-    loadChildren: () => import('./app/pages/auth/auth.routes'),
+    loadChildren: () => import('./pages/auth/auth.routes'),
   },
   { path: '**', redirectTo: '/notfound' },
 ];
