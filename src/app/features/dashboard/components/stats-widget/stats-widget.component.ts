a9f84c9be477dcbcca826f61023707a69e87ffbb
Nick220505 <juannicolaspardo@gmail.com>
2025-04-17 04:34:53 -0500

    Remove unused dashboard components and their associated files to streamline the project structure and improve maintainability.

--- src/app/features/dashboard/components/stats-widget/stats-widget.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-stats-widget',
-  templateUrl: './stats-widget.component.html',
-  styleUrl: './stats-widget.component.css',
-})
-export class StatsWidgetComponent {}
