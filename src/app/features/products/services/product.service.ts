2c2821c6efcc5b435a157921f211803220a2d1ea
Nick220505 <juannicolaspardo@gmail.com>
2025-04-20 16:34:47 -0500

    Refactor TypeScript Configuration and Update Service Imports
    - Changed baseUrl in tsconfig.json from './src/app' to './src' for improved path resolution.
    - Updated path mappings for '@core/*' and '@features/*' to reflect the new directory structure.
    - Modified imports in category.service.ts and product.service.ts to use the new '@env' alias for environment configuration, enhancing code clarity and maintainability.

--- src/app/features/products/services/product.service.ts
+++ src/app/features/products/services/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { environment } from '@env';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment.development';
 import {
   CreateProductDto,
   Product,
