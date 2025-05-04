f69df6490add3137b407d75afaa0ba332ac6bd24
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-05-04 00:25:57 -0500

    refactor(auth): update environment import path in AuthService for consistency
    Modified the import path for the environment configuration in AuthService to align with the new structure, enhancing consistency across the authentication module. This change improves clarity and maintainability of the codebase.

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { environment } from '@env';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment';
 import { AuthResponse } from '../models/auth-response.model';
 import { LoginRequest } from '../models/login-request.model';
 import { RegisterRequest } from '../models/register-request.model';
