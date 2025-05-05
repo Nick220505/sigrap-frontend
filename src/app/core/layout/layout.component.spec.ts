d717b740cd7d8ec55d0bf85663de79af1ae260f4
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-05-05 01:16:48 -0500

    refactor(tests): simplify LayoutComponent test configuration by removing unnecessary imports and providers
    Streamlined the test setup for LayoutComponent by eliminating unused imports and providers, enhancing clarity and maintainability of the test suite.

--- src/app/core/layout/layout.component.spec.ts
+++ src/app/core/layout/layout.component.spec.ts
@@ -1,15 +1,4 @@
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { ToastModule } from 'primeng/toast';
-import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { FloatingConfiguratorComponent } from './components/topbar/floating-configurator/floating-configurator.component';
-import { TopbarComponent } from './components/topbar/topbar.component';
 
 import { LayoutComponent } from './layout.component';
 
@@ -19,23 +8,7 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        LayoutComponent,
-        NoopAnimationsModule,
-        SidebarComponent,
-        TopbarComponent,
-        FooterComponent,
-        FloatingConfiguratorComponent,
-        ToastModule,
-        ConfirmDialogModule,
-      ],
-      providers: [
-        provideHttpClient(),
-        provideHttpClientTesting(),
-        provideRouter([]),
-        ConfirmationService,
-        MessageService,
-      ],
+      imports: [LayoutComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LayoutComponent);
